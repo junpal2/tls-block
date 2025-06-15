34e418fa53619959f9147e151eb81ddbad1aaf28
junpal2 <lily050402@gmail.com>
2025-06-15 16:07:24 +0000

    Add iphdr.h

--- iphdr.h
+++ iphdr.h
@@ -6,24 +6,27 @@
 
 #pragma pack(push, 1)
 struct IpHdr final {
-    uint8_t ip_v:4;
-    uint8_t ip_len:4;
-    uint8_t tos;
-    uint16_t total_len;
-    uint16_t id;
-    uint8_t frag_offset:5;
-    uint8_t more_fragment:1;
-    uint8_t dont_fragment:1;
-    uint8_t reserved_zero:1;
-    uint8_t frag_offset2;
-    uint8_t ttl;
-    uint8_t proto;
-    uint16_t check;
-    Ip sip_;
-    Ip dip_;
+	uint8_t version_ihl;
+	uint8_t tos;
+	uint16_t total_len;
+	uint16_t id;
+	uint8_t frag_offset:5;
+	uint8_t more_fragment:1;
+	uint8_t dont_fragment:1;
+	uint8_t reserved_zero:1;
+	uint8_t frag_offset2;
+	uint8_t ttl;
+	uint8_t proto;
+	uint16_t check;
+	Ip sip_;
+	Ip dip_;
 
-    Ip sip() { return ntohl(sip_); }
+	Ip sip() { return ntohl(sip_); }
 	Ip dip() { return ntohl(dip_); }
 };
 typedef IpHdr* PIpHdr;
 #pragma pack(pop)
+
+inline int ip_hdr_len(const IpHdr* ip){
+	return (ip->version_ihl&0x0F)*4;
+}
